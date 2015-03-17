f513b0f7d3f558d5221c983db9af98ff24c8d7f9
Evren Esat Özkan <mail@my.blog.uri>
2015-03-17 11:35:14 +0200

    servicetasks are working but CompactWorkflowSerializer not working for its test   #4

--- tests/SpiffWorkflow/pytests/test_servicetask.py
+++ tests/SpiffWorkflow/pytests/test_servicetask.py
@@ -12,13 +12,13 @@ from tests.SpiffWorkflow.pytests.testengine import BpmnTestEngine
 __author__ = "Evren Esat Ozkan"
 
 
-def test_subprocess_with_usertask():
-    we = BpmnTestEngine('main_proccess_id', 'subprocess.bpmn')
-    we.save_restore()
+def test_servicetask_with_class():
+    we = BpmnTestEngine('Process_1', 'servicetask_class.bpmn')
+    # we.save_restore()
+    we.get_named_step('StartEvent_1').complete()
+    service_task = we.get_named_step('ServiceTask_class')
+    assert 'views.module.ViewClass' == service_task.task_spec.service_class
+    # we.save_restore()
     we.workflow.do_engine_steps()
-    we.save_restore()
-    print we.workflow.task_tree.get_dump()
-    we.do_next_exclusive_step('Do Something')
-    we.save_restore()
-    we.workflow.do_engine_steps()
+    # print we.workflow.task_tree.get_dump()
     assert 0 == len(we.workflow.get_tasks(Task.READY | Task.WAITING))
